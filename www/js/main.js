c945f16c1ab59f9b4026dfb87b0c51925dd922c8
Arik Shitrit <arik.shitrit@gmail.com>
2013-03-09 22:33:57 +0200

    add alert

--- www/js/main.js
+++ www/js/main.js
@@ -14,12 +14,23 @@ var app = {
     },
 
     initialize: function() {
-//        this.store = new MemoryStore();
-//        this.store = new LocalStorageStore();
-        this.store = new WebSqlStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
-
+	var self = this;
+	this.store = new MemoryStore(function() {
+//	this.store = new LocalStorageStore(function() {
+//	this.store = new WebSqlStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+	});
+	$('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    
+    showAlert: function (message, title) {
+	if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+	    alert(title ? (title + ": " + message) : message);
+	} else {
+            alert(title ? (title + ": " + message) : message);
+	}
+    },
 };
 
 app.initialize();
